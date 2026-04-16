7955378d2360ce8434d53612b265f60b93090711
Arley Gomez <arley.gomez@gmail.com>
2026-04-16 18:59:51 -0500

    Clase 11 - Rutas Anidadas y Lazy Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,55 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import './App.css'
-import Product from './components/Product'
-import Home from './components/Home'
+import { Suspense } from "react";
+import { Routes, Route, Link } from "react-router-dom"
+import Product from "./components/Product"
+import Home from "./components/Home"
+import Dashboard from "./components/DashBoard"
+import Overview from "./components/Overview"
+import Settings from "./components/Settings"
 
 function App() {
-  
-  return (
-    <>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/product/1">Product 1</Link>
-            </li>
-            <li>
-              <Link to="/product/2">Product 2</Link>
-            </li>
-          </ul>
-        </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product/:id" element={<Product  />} />
-        </Routes>
-      </div>
-    </>
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/product/:id" element={<Product />}></Route>
+        <Route path="/dashboard/*" element={<Dashboard />}>
+          <Route path="overview" element={<Overview />}></Route>
+          <Route path="settings" element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Settings />
+            </Suspense>
+          }>
+          </Route>
+        </Route>
+        
+        
+      </Routes>
+
+    </div>
   )
 }
 
 function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  )
+  return <h2>About</h2>
 }
 
 export default App
